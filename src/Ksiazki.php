77ddcd10205e57969eb6179f4c03bfa857398e8a
pawelkotas <pawel.kotas@tlen.pl>
2021-04-01 00:41:06 +0200

    Dodano polaczenie join do tabel autorzy i kategorie w klasie ksiazki Dodano imie i nazwisko autora oraz nazwe kategorii do listy

--- src/Ksiazki.php
+++ src/Ksiazki.php
@@ -23,7 +23,9 @@ class Ksiazki
 	 */
 	public function pobierzWszystkie(): ?array
     {
-		$sql = "SELECT k.* FROM ksiazki k  ";
+		$sql = "SELECT k.*, a.imie, a.nazwisko, kat.nazwa FROM ksiazki k 
+                JOIN autorzy a ON k.id_autora=a.id
+                JOIN kategorie kat ON k.id_kategorii=kat.id               ";
 
 		return $this->db->pobierzWszystko($sql);
 	}

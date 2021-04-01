d1e58a003f92966e08959f5655db0ba8c9f71091
pawelkotas <pawel.kotas@tlen.pl>
2021-04-01 11:27:29 +0200

    lab2 zad 2 - rozbudowano sekcję "szczegóły"

--- ksiazki.szczegoly.php
+++ ksiazki.szczegoly.php
@@ -21,7 +21,40 @@ $dane = $ksiazki->pobierz($id);
 <p>
 	<a href="ksiazki.lista.php"><i class="fas fa-chevron-left"></i> Powrót</a>
 </p>
+<table class="table table-striped table-condensed">
+    <tbody>
+        <tr>
+            <td style="width: 300px"; rowspan="6">
+                <?php if (!empty($dane['zdjecie'])) : ?>
+                    <img src="zdjecia/<?= $dane['zdjecie'] ?>" alt="<?= $dane['tytul'] ?>" class="img-thumbnail" />
+                <?php else : ?>
+                    brak zdjęcia
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr style="font-size: 16px; font-weight:bold">
+            <td>Tytuł:</td>
+            <td><?=$dane['tytul']?></td>
+        </tr>
+        <tr>
+            <td>Cena:</td>
+            <td><?=$dane['cena']?> zł</td>
+        </tr>
+        <tr>
+            <td>Liczba stron:</td>
+            <td><?=$dane['liczba_stron']?></td>
+        </tr>
+            <td>ISBN:</td>
+            <td><?=$dane['isbn']?></td>
+        </tr>
+        <tr>
+            <td>Opis:</td>
+            <td><?=$dane['opis']?></td>
+        </tr>
+    </tbody>
 
-<p>szczegóły książki......</p>
+
+
+</table>
 
 <?php include 'footer.php'; ?>

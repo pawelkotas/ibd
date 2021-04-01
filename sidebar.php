e60054e42133f687d5286a1900864cb1c080994b
pawelkotas <pawel.kotas@tlen.pl>
2021-04-01 17:25:40 +0200

    uzupełniono sidebar dla bestsellerów

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,27 @@
+<?php
+use Ibd\Ksiazki;
+$ksiazki = new Ksiazki();
+$bestsellery = $ksiazki->pobierzBestsellery();
+?>
+
 <div class="col-md-2">
 	<h1>Bestsellery</h1>
-	
-	<ul>
-		<li>Książka 1</li>
-		<li>Książka 2</li>
-		<li>Książka 3</li>
-		<li>Książka 4</li>
-		<li>Książka 5</li>
-	</ul>
+    <table class="table">
+        <?php foreach($bestsellery as $bs) : ?>
+            <tr>
+                <td>
+                    <a href="ksiazki.szczegoly.php?id=<?= $bs['id'] ?>" title="szczegóły">
+                    <?php if (!empty($ks['zdjecie'])) : ?>
+                        <img src="zdjecia/<?= $bs['zdjecie'] ?>" alt="<?= $bs['tytul'] ?>" class="img-thumbnail" />
+                    <?php else : ?>
+                        brak zdjęcia
+                    <?php endif; ?>
+                        <h6><?=$bs['tytul']?></h6>
+                        <?=$bs['imie']?>
+                        <?=$bs['nazwisko']?>
+                    </a>
+                </td>
+            </tr>
+        <?php endforeach;?>
+    </table>
 </div>
